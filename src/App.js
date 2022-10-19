20e15a038d9d9ae0adcb2140ca2e47817c231718
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2022-10-19 15:54:57 +0545

    Developed by Ankit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './index.css'
 
-function App() {
+const App = ()=> {
+  const[p,setP] = useState();
+  const[t,setT] = useState();
+  const[r,setR] = useState();
+  const[s,setS] = useState();
+  const calculateInterest = () =>{
+    setS(p*t*r/100)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div'>
+      <AppBar position="static">
+        <Toolbar>
+       
+    
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Simple Interest Calculator
+          </Typography>
+        
+        </Toolbar>
+      </AppBar>
+      <br></br>
+      <br></br>
+      <TextField onChange={(event)=> setP(event.target.value)}
+          id="outlined-number"
+          label="Enter Principal amount"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <br></br>
+        <br></br>
+        <TextField onChange={(event)=> setT(event.target.value)}
+          id="outlined-number"
+          label="Enter Time"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <br></br>
+        <br></br>
+        <TextField onChange={(event)=> setR(event.target.value)}
+          id="outlined-number"
+          label="Enter Rate"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <br></br>
+        <br></br>
+        <Button onClick={()=> calculateInterest()} variant="contained">Calculate</Button>
+        <br></br>
+        <br></br>
+      <Typography variant="h6" gutterBottom>
+        Simple Interest is : {s}
+      </Typography>
     </div>
   );
 }
